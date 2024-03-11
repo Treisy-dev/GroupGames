3a6584b0898834b95731bcbb5810c92c14a9f13b
Treisy-dev <vip.protop2004@gmail.com>
2024-03-11 12:27:08 +0300

    Add default app config

---
+++ GroupGames/GroupGames/MainScreen/MainView.swift
@@ -0,0 +1,20 @@
+//
+//  MainView.swift
+//  GroupGames
+//
+//  Created by Кирилл Щёлоков on 11.03.2024.
+//
+
+import UIKit
+
+class MainView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
